7f58b69d71f0ca0339edfe741f43efb9b94afdf6
Micael Ferreira <micaelmf@gmail.com>
2024-09-10 15:12:17 -0300

    feat: adding template for tasks list

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,19 +5,23 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>@yield('title', 'MyTasks')</title>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
+    <title>@yield('title', 'MyTasks')</title>
+
+    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.8/css/line.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ asset('css/main.css') }}">
     <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
+
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"
+        integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
 </head>
 
 <body>
-    <header>
-        <h1>Minhas tarefas</h1>
-    </header>
-
-    <main>
+    <main class="container mt-4">
         @yield('content')
     </main>
 
--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -3,5 +3,68 @@
 @section('title', 'Minhas tarefas')
 
 @section('content')
-    Olá
+    <div class="submenu">
+        <div class="submenu-left">
+            <h2>Lista</h2>
+        </div>
+        <div class="submenu-right">
+            <form class="filter-form" action="" method="GET">
+                <input type="text" name="search" placeholder="Buscar">
+                <button class="btn btn-outline-primary" type="submit"><i class="uil uil-search"></i></button>
+            </form>
+            <a class="btn btn-primary" href="{{ route('tasks.create') }}"><i class="uil uil-plus text-center"></i></a>
+        </div>
+    </div>
+
+    <div class="task-list">
+        @foreach ($tasks ?? [] as $task)
+            <div class="task" id="{{ $task->id }}">
+                <div class="task-info">
+                    <span class="task-id">{{ $task->id }}</span>
+                    <span class="task-name">{{ $task->name }}</span>
+                    <span class="task-date">{{ $task->date }}</span>
+                    <span class="task-status">{{ $task->status }}</span>
+                </div>
+                <div class="task-actions">
+                    <button type="button" href="{{ route() }}" class="btn btn-danger"><i
+                            class="uil uil-trash"></i></button>
+                    <a href="{{ route('tasks.edit') }}" class="btn btn-secondary"><i class="uil uil-pen"></i></a>
+                    <button type="button" class="btn btn-primary"><i class="uil uil-check"></i></button>
+                </div>
+            </div>
+        @endforeach
+    </div>
+
+    <script>
+        $(document).ready(function() {
+            $('.complete-task').on('click', function() {
+                let taskId = $(this).data('id');
+                const data = {
+                    _token: '{{ csrf_token() }}',
+                    status: 'completed'
+                };
+
+                $.ajax({
+                    url: '/tasks/' + taskId + '/edit',
+                    type: 'PATCH',
+                    data: data,
+                    beforeSend: function() {
+                        $('#task-' + taskId + ' .task-status').text('loading...');
+                    },
+                    success: function(response) {
+                        if (response.success) {
+                            $('#task-' + taskId + ' .task-status').text('completed');
+                            alert(response.message);
+                        }
+                    },
+                    afterSend: function() {
+                        $('#task-' + taskId + ' .task-status').text('completed');
+                    },
+                    error: function() {
+                        $('#task-' + taskId + ' .task-status').text('error');
+                    }
+                });
+            });
+        });
+    </script>
 @endsection
