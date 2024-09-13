fcb04a8508e1fb732d315cc283c912ecf13bf429
Micael Ferreira <micaelmf@gmail.com>
2024-09-13 10:06:09 -0300

    refactor: add seeders to tasks and users

---
+++ database/factories/TaskFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Nette\Utils\Random;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Task>
+ */
+class TaskFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->sentence,
+            'description' => $this->faker->paragraph,
+            'due_date' => $this->faker->date,
+            'status' => $this->faker->randomElement(['pending', 'doing', 'completed']),
+            'deleted_at' => Random::generate(1) == 1 ? now() : null,
+        ];
+    }
+}
