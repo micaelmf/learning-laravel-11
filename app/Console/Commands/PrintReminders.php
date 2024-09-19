cb87df2aa1a48900e8a64a3dd589b33ddc3d381f
Micael Ferreira <micaelmf@gmail.com>
2024-09-19 16:31:37 -0300

    configure kernel.php

---
+++ app/Console/Commands/PrintReminders.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Reminder;
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+
+class PrintReminders extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:print-reminders';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Print reminders that are due';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $now = Carbon::now();
+        $reminders = Reminder::where('status', '!=', 'doing')
+            ->where('status', '!=', 'archived')
+            ->where('reminder_time', '<', $now)
+            ->get();
+
+        foreach ($reminders as $reminder) {
+            $this->info($reminder);
+        }
+
+        return 0;
+    }
+}
